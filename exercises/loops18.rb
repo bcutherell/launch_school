1086f0dd8b0591bf66ff50532dd64e417ceaea21
Blake Cutherell <bcutherell@gmail.com>
2023-12-02 19:09:47 -0600

    more practice

---
+++ exercises/loops18.rb
@@ -0,0 +1,18 @@
+number = 0
+
+until number == 10
+	number += 1
+	next if number.odd?
+	puts number
+end
+
+
+
+# number = 0
+
+# until number == 10
+# 	number += 1
+# 	if number.even?
+# 		puts number
+# 	end
+# end
