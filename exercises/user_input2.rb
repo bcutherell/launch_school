c4d4335b523b9be6e1919873dd440161e4d19e61
Blake Cutherell <bcutherell@gmail.com>
2023-12-04 18:31:30 -0600

    practice

---
+++ exercises/user_input2.rb
@@ -0,0 +1,12 @@
+puts ">> What is your age in years?"
+age_in_years = gets
+age_in_months = 12 * age_in_years.to_i
+puts "You are #{age_in_months} months old."
+
+
+# my original solution, it worked
+
+# puts ">> What is your age in years?"
+# age = gets
+
+# puts "You are #{age * 12} months old"
