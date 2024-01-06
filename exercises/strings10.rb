eda8f215d597cd094af2fe67c8ce615cfe77e91e
Blake Cutherell <bcutherell@gmail.com>
2024-01-06 10:13:39 -0600

    strings practice

---
+++ exercises/strings10.rb
@@ -0,0 +1,14 @@
+colors = 'blue boredom yellow'
+
+
+puts colors.include?('red')
+
+# returns true because boredom contains the letters 'red'
+
+
+
+
+# colors = 'blue pink yellow orange'
+
+# puts colors.include?('yellow')
+# puts colors.include?('purple')
---
+++ exercises/strings9.rb
@@ -0,0 +1,9 @@
+words = 'car human elephant airplane'
+
+words.split(' ').each do |word|
+puts word + 's'
+end
+
+# words = ['car', 'human', 'elephant', 'airplane']
+
+# puts words.each {|x| puts "#{x}""s"}
