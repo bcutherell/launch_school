2582e672e28c08ee4743b485e53a7e32379e0153
Blake Cutherell <bcutherell@gmail.com>
2024-01-07 09:31:31 -0600

    array practice

---
+++ exercises/arrays5.rb
@@ -0,0 +1,10 @@
+colors = ['red', 'yellow', 'purple', 'green']
+
+colors.each do |x| 
+	puts "I'm the color #{x}!"
+end
+
+
+# colors.each {|x| puts "I'm the color #{x}!"}
+
+# colors.each {|x| puts "I'm the color " + x + "!"}
