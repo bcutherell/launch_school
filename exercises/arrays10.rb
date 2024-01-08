37a1f2e339059120a166355028564f378c440d1a
Blake Cutherell <bcutherell@gmail.com>
2024-01-08 11:11:44 -0600

    arrays and hash practice

---
+++ exercises/arrays10.rb
@@ -0,0 +1,9 @@
+array1 = [1, 5, 9]
+array2 = [1, 9, 5]
+
+puts array1 == array2
+
+
+# torf = array1 == array2
+
+# puts torf
--- exercises/arrays8.rb
+++ exercises/arrays8.rb
@@ -1,1 +1,8 @@
-[['Dave', 7,] ['Miranda', 3,] ['Jason', 11]]
+list = [['Dave', 7], ['Miranda', 3], ['Jason', 11]]
+
+
+# list = [['Dave', 7,] ['Miranda', 3,] ['Jason', 11]] 
+
+
+p list
+# comma's in the wrong spot.
