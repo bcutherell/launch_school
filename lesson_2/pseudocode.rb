4f235a1100a84b5784fa2af1b29563f9c006664e
Blake Cutherell <bcutherell@gmail.com>
2024-01-11 14:20:09 -0600

    pseudocode practice

---
+++ lesson_2/pseudocode.rb
@@ -0,0 +1,38 @@
+# a method that returns the sum of two integers
+
+Given two numbers.
+adds bother numbers together and returns the sum.
+
+START
+
+METHOD call it addition
+ take number one and number two and add them together
+ END
+
+GET number1
+GET number2
+
+answer = number1 PLUS number2
+
+PRINT answer
+
+
+# a method that takes an array of strings, and returns a string that is all those strings concatenated together
+
+Given an array of strings.
+return a string that is all those seperate strings concatenated
+
+START
+
+ARRAY = STRINGS
+
+METHOD concatenate
+ array.concatenate
+ END
+
+PRINT METHOD(ARRAY)
+
+# a method that takes an array of integers, and returns a new array with every other element 
+# from the original array, starting with the first element. For instance:
+
+I have no idea.
