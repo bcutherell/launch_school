9162ebd452eaddd505ea48be652e892c1df4a2ff
Blake Cutherell <bcutherell@gmail.com>
2024-01-22 14:18:34 -0600

    medium and hard problems

---
+++ lesson_3/medium_2/three.rb
@@ -0,0 +1,15 @@
+def tricky_method(string_param_one, string_param_two)
+  string_param_one += "rutabaga"
+  string_param_two << "rutabaga"
+end
+
+string_arg_one = "pumpkins"
+string_arg_two = "pumpkins"
+tricky_method(string_arg_one, string_arg_two)
+
+puts "String_arg_one looks like this now: #{string_arg_one}"
+puts "String_arg_two looks like this now: #{string_arg_two}"
+
+# I believe one will stay unchanged and
+# two will change to pumpkins rutabaga
+# += is non mutating and << is mutating
---
+++ lesson_3/medium_2/two.rb
@@ -0,0 +1,7 @@
+a = nil
+b = nil
+c = a
+
+puts a.object_id
+puts b.object_id
+puts c.object_id
