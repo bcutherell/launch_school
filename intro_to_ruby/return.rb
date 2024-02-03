b60eaa540212584241d56f3581451ca37903510c
Blake Cutherell <bcutherell@gmail.com>
2024-02-03 02:15:31 -0600

    second pass

---
+++ intro_to_ruby/return.rb
@@ -0,0 +1,12 @@
+def just_assignment(number)
+  foo = number + 3
+end
+
+puts just_assignment(2)
+
+# def add_three(number)
+#   return number + 3
+# end
+
+# returned_value = add_three(4)
+# puts returned_value
