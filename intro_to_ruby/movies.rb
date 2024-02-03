5e7068a83df71a1fd39769dcd6159c8e1aac861e
Blake Cutherell <bcutherell@gmail.com>
2024-02-03 01:34:41 -0600

    second pass

---
+++ intro_to_ruby/movies.rb
@@ -0,0 +1,9 @@
+movies = {:jaws => '1984',
+          :alien => '1999',
+          :pulp_fiction => '1996',
+          :up => '2010'}
+
+puts movies[:jaws]
+puts movies[:alien]
+puts movies[:pulp_fiction]
+puts movies[:up]
