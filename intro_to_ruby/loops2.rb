542811526515dd30598e3f27d04d9f1c74f76951
Blake Cutherell <bcutherell@gmail.com>
2024-02-05 14:52:42 -0600

    second pass

---
+++ intro_to_ruby/loops2.rb
@@ -0,0 +1,13 @@
+answer = ""
+while answer != 'STOP'
+  puts "Give me some input or type STOP."
+  answer = gets.chomp
+end
+
+# loop do
+#   puts "Give me some input or type STOP."
+#   answer = gets.chomp
+#   if answer == 'STOP'
+#     break
+#   end
+# end
