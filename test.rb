7cbe1b6673b0768097c25bd455f2cc5e6e05f3ca
Blake Cutherell <bcutherell@gmail.com>
2024-02-08 22:09:39 -0600

    study group question

--- test.rb
+++ test.rb
@@ -1,7 +1,28 @@
-numbers = [1, 2, 3]
+# What code could we add to see each student printed with their respective grade?
 
-def print_numbers(count)
-  puts numbers.to_s * count
+students = ['Martin', 'Milo', 'Michelle']
+grades = [94, 87, 98]
+
+index = 0
+while index < students.length
+  puts '------------'
+  puts students[index]
+  puts grades[index]
+  index += 1
 end
 
-print_numbers
+
+
+
+
+
+
+
+#####
+
+# # Can you write the code necessary to get the desired output shown on the last line?
+# make = 'Ford'
+# model = 'Ranger'
+
+# puts # Your Code Here
+# # Look! It's a Ford Ranger!
