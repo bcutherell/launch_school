fc8ca574c6f159a500fcd2f63c06db9e50eb5828
Blake Cutherell <bcutherell@gmail.com>
2024-02-20 18:06:55 -0600

    practice

--- test.rb
+++ test.rb
@@ -1,50 +1,110 @@
-arr = [1]
-
-for i in arr do
-  a = 'World'
-end
-
-puts a
 
 
 
 
+## These two gave me trouble, look into them
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# What code could we add to see each student printed with their respective grade?
-
-# students = ['Martin', 'Milo', 'Michelle']
-# grades = [94, 87, 98]
-
-# index = 0
-# while index < students.length
-#   puts '------------'
-#   puts students[index]
-#   puts grades[index]
-#   index += 1
+# def test(str)
+#   str = str.upcase!
+#   str.downcase!
 # end
 
+# test_str = 'Written Assessment'
+# test(test_str)
+# puts test_str
 
-#####
 
-# # Can you write the code necessary to get the desired output shown on the last line?
-# make = 'Ford'
-# model = 'Ranger'
+# def test(str)
+#   str += '!'
+#   str.downcase!
+# end
 
-# puts # Your Code Here
-# # Look! It's a Ford Ranger!
+# test_str = 'Written Assessment'
+# test(test_str)
+
+# puts test_str
+
+
+
+
+
+
+
+# a = 4
+# b = 2
+
+# loop do
+#   c = 3
+#   a = c
+#   break
+# end
+
+# puts a
+# puts b
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# arr = [1]
+
+# for i in arr do
+#   a = 'World'
+# end
+
+# puts a
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# # What code could we add to see each student printed with their respective grade?
+
+# # students = ['Martin', 'Milo', 'Michelle']
+# # grades = [94, 87, 98]
+
+# # index = 0
+# # while index < students.length
+# #   puts '------------'
+# #   puts students[index]
+# #   puts grades[index]
+# #   index += 1
+# # end
+
+
+# #####
+
+# # # Can you write the code necessary to get the desired output shown on the last line?
+# # make = 'Ford'
+# # model = 'Ranger'
+
+# # puts # Your Code Here
+# # # Look! It's a Ford Ranger!
