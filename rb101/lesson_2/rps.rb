0c4fd7f475243c0ad53db897b51c5bcca7614352
Blake Cutherell <bcutherell@gmail.com>
2024-03-03 00:32:29 -0600

    second pass

--- rb101/lesson_2/rps.rb
+++ rb101/lesson_2/rps.rb
@@ -1,17 +1,18 @@
-VALID_CHOICES = ['rock', 'paper', 'scissors']
-
+VALID_CHOICES = %w(rock paper scissors)
 def prompt(message)
   Kernel.puts("=> #{message}")
 end
 
+def win?(first, second)
+  (first == 'rock' && second == 'scissors') ||
+    (first == 'paper' && second == 'rock') ||
+    (first == 'scissors' && second == 'paper')
+end
+
 def display_results(player, computer)
-  if (player == 'rock' && computer == 'scissors') ||
-      (player == 'paper' && computer == 'rock') ||
-      (player == 'scissors' && computer == 'paper')
+  if win?(player, computer)
     prompt("You won!")
-  elsif (player == 'rock' && computer == 'paper') ||
-        (player == 'paper' && computer == 'scissors') ||
-        (player == 'scissors' && computer == 'rock')
+  elsif win?(computer, player)
     prompt("Computer won!")
   else
     prompt("It's a tie!")
@@ -21,8 +22,8 @@ end
 loop do # main loop
   choice = ''
   loop do
-  prompt("Choose one: #{VALID_CHOICES.join(', ')}")
-  choice = Kernel.gets().chomp()
+    prompt("Choose one: #{VALID_CHOICES.join(', ')}")
+    choice = Kernel.gets().chomp()
 
     if VALID_CHOICES.include?(choice)
       break
