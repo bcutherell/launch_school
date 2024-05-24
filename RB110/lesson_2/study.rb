9b9577d34e68ea0b6ee87b2b0e4d84afe8ea61ec
Blake Cutherell <bcutherell@gmail.com>
2024-05-23 19:47:53 -0500

    RB110 practice

--- RB110/lesson_2/study.rb
+++ RB110/lesson_2/study.rb
@@ -1,9 +1,9 @@
-sample = [[[1], [2], [3], [4]], [['a'], ['b'], ['c']]].map do |element1|
-  element1.select do |element2|
-    element2.partition do |element3|
-      element3.size > 2
-    end
-  end
-end
+order_data = [
+  {customer_id: 12, customer_name: 'Emma Lopez', order_id: 351, order_date: '12/04/16', order_fulfilled: true, order_value: 135.99},
+  {customer_id: 12, customer_name: 'Emma Lopez', order_id: 383, order_date: '12/04/16', order_fulfilled: true, order_value: 289.49},
+  {customer_id: 12, customer_name: 'Emma Lopez', order_id: 392, order_date: '01/10/17', order_fulfilled: false, order_value: 58.00},
+  {customer_id: 32, customer_name: 'Michael Richards', order_id: 241, order_date: '11/10/16', order_fulfilled: true, order_value: 120.00},
+  {customer_id: 32, customer_name: 'Michael Richards', order_id: 395, order_date: '01/10/17', order_fulfilled: false, order_value: 85.65},
+  # rest of data...
+]
 
-puts sample
