ad3abbb89505fa096b96b105729e7645e900f238
Blake Cutherell <bcutherell@gmail.com>
2024-06-04 16:11:09 -0500

    RB110 practice

--- RB110/101-119_small_problems/easy_5/letter_swap.rb
+++ RB110/101-119_small_problems/easy_5/letter_swap.rb
@@ -1,15 +1,21 @@
+require 'pry-byebug'
+
 def swap(string)
   arr = []
   string_arr = string.split
   string_arr.each do |word|
-    arr << word.pop
-    arr << word[1..-1]
-    arr << word[0]
+    if word.length == 1
+       arr << word
+    elsif word.length == 2
+       arr << word.reverse
+    else
+      word[0], word[-1] = word[-1], word[0]
+      arr << word
+    end
   end
-  arr.join(' ').to_s
+  arr.join(' ')
 end
 
 p swap('Oh what a wonderful day it is')
 p swap('Abcde')
 p swap('a')
-
--- RB110/example.rb
+++ RB110/example.rb
@@ -1,23 +1,43 @@
-require 'pry-byebug'
+# require 'pry-byebug'
 
-def count_letters(string, char)
-  string.downcase.count(char.downcase)
-end
+# def count_letters(string, char)
+#   string.downcase.count(char.downcase)
+# end
 
-def single_letters(string)
-  binding.pry
-  singles = []
-  string.chars.each do |char|
-    letter_count = count_letters(string, char)
+# def single_letters(string)
+#   binding.pry
+#   singles = []
+#   string.chars.each do |char|
+#     letter_count = count_letters(string, char)
 
-    if letter_count == 1
-      singles << char.downcase
-    end
+#     if letter_count == 1
+#       singles << char.downcase
+#     end
+#   end
+
+#   singles
+# end
+
+# p single_letters('abc') == ['a', 'b', 'c']
+# p single_letters('Hello World') == ['h', 'e', ' ', 'w', 'r', 'd']
+# p single_letters('Cool cat') == ['l', ' ', 'a', 't']
+
+
+def multiply(arr, multiplier)
+  doubled_numbers = []
+  counter = 0
+
+  loop do
+    break if counter == arr.size
+
+    current_number = arr[counter]
+    doubled_numbers << current_number * multiplier
+
+    counter += 1
   end
 
-  singles
+  doubled_numbers
 end
 
-p single_letters('abc') == ['a', 'b', 'c']
-p single_letters('Hello World') == ['h', 'e', ' ', 'w', 'r', 'd']
-p single_letters('Cool cat') == ['l', ' ', 'a', 't']
+my_numbers = [1, 4, 3, 7, 2, 6]
+p multiply(my_numbers, 3)
