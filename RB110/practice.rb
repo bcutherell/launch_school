8f4df91c5b05c7a3ab23c6ab73a7e6eb48b67592
Blake Cutherell <bcutherell@gmail.com>
2024-06-06 18:52:32 -0500

    practice

---
+++ RB110/practice.rb
@@ -0,0 +1,46 @@
+# flintstones = ["Fred", "Barney", "Wilma", "Betty", "Pebbles", "BamBam"]
+# flintstones_hash = {}
+# counter = 0
+
+# flintstones.each_with_index do |element, index|
+#   flintstones_hash[element] = index
+# end
+
+# flintstones.each do |name|
+#   flintstones_hash[name] = counter
+#   counter += 1
+# end
+
+# p flintstones_hash
+#-----------------------------
+# ages = { "Herman" => 32, "Lily" => 30, "Grandpa" => 5843, "Eddie" => 10, "Marilyn" => 22, "Spot" => 237 }
+# ages_total = 0
+
+# ages.values.each do |age|
+#   ages_total += age
+# end
+
+# ages.each do |name, age|
+#   ages_total += age
+# end
+
+# p ages_total
+#-----------------------------
+# ages = { "Herman" => 32, "Lily" => 30, "Grandpa" => 402, "Eddie" => 10 }
+
+# p ages.reject { |key, value| value >= 100 }
+# p ages.select { |key, value| value < 100}
+# p ages.delete_if { |key, value| value >= 100 }
+#------------------------------
+
+# ages = { "Herman" => 32, "Lily" => 30, "Grandpa" => 5843, "Eddie" => 10, "Marilyn" => 22, "Spot" => 237 }
+# current_age = 0
+# p ages.values.min
+
+# loop do
+#   current_age = ages.values
+
+
+
+#-------------------------------
+
