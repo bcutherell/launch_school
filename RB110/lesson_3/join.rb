86d1b417551122d76f1aad4e143ad7b90b3e06f7
Blake Cutherell <bcutherell@gmail.com>
2024-06-11 11:25:55 -0500

    ttt work

--- RB110/lesson_3/join.rb
+++ RB110/lesson_3/join.rb
@@ -7,8 +7,35 @@
 # joinor([1, 2])
 # joinor([1, 2, 3])
 
-arr = [1, 2, 3, 4]
+# arr = [1, 2, 3, 4]
 
-arr.map do |num|
-  num.to_s + ", "
+# arr.map do |num|
+#   num.to_s + ", "
+# end
+
+# def joinor(arr, punct = ', ', extra = 'or')
+# str = []
+#   arr.map do |num|
+#     if num == arr.last
+#       str << num.to_s.prepend(extra + ' ')
+#     else
+#       str << num.to_s
+#     end
+#   end
+# str.join(punct)
+# end
+
+def joinor(arr, punctuation = ', ', delim = 'or')
+  if arr.length <= 1
+    arr.join
+  elsif arr.length == 2
+    arr.join (' or ')
+  else
+    arr[0..-2].join(punctuation) + ', ' + delim + ' ' + arr[-1].to_s
+  end
 end
+
+p joinor([1, 2])
+p joinor([1, 2, 3])
+p joinor([1, 2, 3], '; ')
+p joinor([1, 2, 3], ', ', 'and')
