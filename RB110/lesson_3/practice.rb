4339da7afe6b29938d7cdcf7d9a28fe9a2b442e3
Blake Cutherell <bcutherell@gmail.com>
2024-06-17 18:11:00 -0500

    RB110 practice

---
+++ RB110/lesson_3/practice.rb
@@ -0,0 +1,5 @@
+ages = { "Herman" => 32, "Lily" => 30, "Grandpa" => 5843, "Eddie" => 10, "Marilyn" => 22, "Spot" => 237 }
+total_ages = 0
+ages.each_value { |value| total_ages += value }
+
+p total_ages
