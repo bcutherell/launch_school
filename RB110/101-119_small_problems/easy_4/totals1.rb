0dacf6665d707d4d597b7590229b23c5c5fcfa25
Blake Cutherell <bcutherell@gmail.com>
2024-06-20 20:36:14 -0500

    practice problems

--- RB110/101-119_small_problems/easy_4/totals1.rb
+++ RB110/101-119_small_problems/easy_4/totals1.rb
@@ -1,21 +1,26 @@
-def running_total(array)
-  sum = 0
-  array.map { |value| sum += value }
-end
+# def running_total(arr)
+#   sum = 0
+#   arr.map { |num| sum += num }
+# end
+
 
 p running_total([2, 5, 13])
+p running_total([14, 11, 7, 15, 20])
+p running_total([3])
+p running_total([])
 
-
-
-
-
+# def running_total(arr)
 #   new_arr = []
 #   counter = 0
 
 #   loop do
-#     new_arr << arr[counter]
-#     counter += 1
 #     break if counter == arr.length
+#     new_arr << (arr[counter] + arr[counter - 1])
+#     counter += 1
 #   end
-#     new_arr
+
+#   new_arr
 # end
+
+
+
