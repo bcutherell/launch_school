00a6ba9f0ffadb6e9a30bb2ad1ee7e026e48e433
Blake Cutherell <bcutherell@gmail.com>
2024-09-20 19:34:49 -0500

    finished join method

--- RB110/lesson_3/TicTacToe/join.rb
+++ RB110/lesson_3/TicTacToe/join.rb
@@ -1,10 +1,14 @@
 def joinor(array, delimiter=', ', joining_word='or')
-  last = array.pop
-  # array.join(delimiter) + "#{delimiter} " + joining_word + " " + last.to_s
-  "#{array.join(delimiter)} #{joining_word} #{last.to_s}"
+  if array.length == 1
+    array[0].to_s
+  elsif array.length == 2
+    array.join(" #{joining_word} ")
+  else
+  "#{array[0...-1].join(delimiter)}#{delimiter}#{joining_word} #{array.last}"
+  end
 end
 
-
+p joinor([1])
 p joinor([1, 2])                   # => "1 or 2"
 p joinor([1, 2, 3])                # => "1, 2, or 3"
 p joinor([1, 2, 3], '; ')          # => "1; 2; or 3"
