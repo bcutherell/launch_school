2e2f31d0266e4c1d72bf309ff993fb7d14065725
Blake Cutherell <bcutherell@gmail.com>
2024-10-09 17:01:47 -0500

    RPS work

--- RB101/lesson_2/Rock_Paper_Scissors/rps.rb
+++ RB101/lesson_2/Rock_Paper_Scissors/rps.rb
@@ -4,8 +4,21 @@ WINNING_COMBINATIONS = {
   'paper' => ['rock', 'spock'],
   'scissors' => ['paper', 'lizard'],
   'lizard' => ['spock', 'paper'],
-  'spock' => ['scissors', 'rock']
+  'spock' => ['scissors', 'rock'],
+  # 'r' => ['scissors', 'lizard'],
+  # 'p' => ['rock', 'spock'],
+  # 'sc' => ['paper', 'lizard'],
+  # 'l' => ['spock', 'paper'],
+  # 'sp' => ['scissors', 'rock']
 }
+SHORTHAND = {
+  'r' => 'rock',
+  'p' => 'paper',
+  'sc' => 'scissors',
+  'l' => 'lizard',
+  'sp' => 'spock'
+}
+GRAND_MASTER_SCORE = 3
 
 def prompt(message)
   puts "=> #{message}"
@@ -25,47 +38,74 @@ def display_results(player, computer)
   end
 end
 
-player_score = 0
-computer_score = 0
+# def grand_master?(player_score, computer_score)
+#   if player_score == 3
+#     player_score = 0
+#     computer_score = 0
+#   elsif computer_score == 3
+#     player_score = 0
+#     computer_score = 0
+#   end
+# end
+
+
 loop do # main loop
-  player_choice = ''
-  loop do
-    system('clear')
-    prompt "Welcome to Rock Paper Scissors Lizard Spock!"
-    prompt "The first to 3 total wins, well.... wins!"
+  player_score = 0
+  computer_score = 0
 
-    prompt "Choose one: #{VALID_CHOICES.join(', ')}"
-    player_choice = gets.chomp
+  system('clear')
+  prompt "Welcome to Rock Paper Scissors Lizard Spock!"
+  prompt "The first to 3 total wins is the Grand Master!"
 
-    if VALID_CHOICES.include?(player_choice)
-      break
-    else
-      prompt "That's not a valid choice. Please try again."
+  while player_score < 3 && computer_score < 3
+    player_choice = ''
+
+    loop do
+      prompt "Choose one: #{VALID_CHOICES.join(', ')}"
+      player_choice = gets.chomp.strip
+
+      if VALID_CHOICES.include?(player_choice)
+        break
+      else
+        prompt "That's not a valid choice. Please try again."
+      end
     end
-  end
 
-  computer_choice = VALID_CHOICES.sample
+    computer_choice = VALID_CHOICES.sample
 
-  prompt "You chose: #{player_choice}; the Computer chose: #{computer_choice}"
+    prompt "You chose: #{player_choice}; the Computer chose: #{computer_choice}"
 
-  prompt display_results(player_choice, computer_choice)
+    prompt display_results(player_choice, computer_choice)
 
-  if display_results(player_choice, computer_choice) == "You won!"
-    player_score += 1
-  elsif display_results(player_choice, computer_choice) == "The Computer won!"
-    computer_score += 1
+    if win?(player_choice, computer_choice)
+      player_score += 1
+    elsif win?(computer_choice, player_choice)
+      computer_score += 1
+    end
+
+    prompt "The current score is Player: #{player_score} and Computer: #{computer_score}"
+
   end
 
   if player_score == 3
     prompt "**The Player has bested the Computer!**"
+    prompt "**The Player is the Grand Master!"
   elsif computer_score == 3
     prompt "**The Computer has bested the Player!**"
+    prompt "**The Computer is the Grand Master!"
   end
 
-  prompt "The current score is Player: #{player_score} and Computer: #{computer_score}"
   prompt "Would you like to play again? (y/n)" # Can't figure out how to not have this after every move.
-  answer = gets.chomp
+  answer = gets.chomp.strip
   break unless answer.downcase.start_with?('y')
+
+    # if player_score == 3
+    #   player_score = 0
+    #   computer_score = 0
+    # elsif computer_score == 3
+    #   player_score = 0
+    #   computer_score = 0
+    # end
 end
 
 prompt "Thank you for playing. Goodbye!"
