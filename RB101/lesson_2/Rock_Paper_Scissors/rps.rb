b20af1e0f308a18e5dc56c54b789c66f36193685
Blake Cutherell <bcutherell@gmail.com>
2024-10-11 08:07:33 -0500

    rps work

--- RB101/lesson_2/Rock_Paper_Scissors/rps.rb
+++ RB101/lesson_2/Rock_Paper_Scissors/rps.rb
@@ -5,14 +5,14 @@ VALID_CHOICES = {
   'paper' => 'paper',       'p' => 'paper',
   'scissors' => 'scissors', 'sc' => 'scissors',
   'lizard' => 'lizard',     'l' => 'lizard',
-  'spock' => 'spock',       'sp' =>'spock'
+  'spock' => 'spock',       'sp' => 'spock'
 } # I don't know how to make this shorter, this feels unnecessarily long.
 WINNING_COMBINATIONS = {
   'rock' => ['scissors', 'lizard'],
   'paper' => ['rock', 'spock'],
   'scissors' => ['paper', 'lizard'],
   'lizard' => ['spock', 'paper'],
-  'spock' => ['scissors', 'rock'],
+  'spock' => ['scissors', 'rock']
 }
 
 def prompt(message)
@@ -45,7 +45,7 @@ def grand_master?(player_score, computer_score)
   player_score >= 3 || computer_score >= 3
 end
 
-def master_announcement(player_score, computer_score)
+def master_announcement(player_score)
   if player_score == 3
     prompt_text("master_player")
   else
@@ -59,29 +59,26 @@ loop do # main loop
 
   system('clear')
   prompt_text("welcome")
-  # prompt "Welcome to Rock Paper Scissors Lizard Spock!"
-  # prompt "The first to 3 total wins is the Grand Master!"
-  # prompt "The current score is Player: #{player_score} and Computer: #{computer_score}"
 
   loop do
     player_choice = ''
     loop do
-      prompt "Choose one: (r)ock, (p)aper, (sc)issors, (l)izard, (sp)ock"
+      prompt_text("choose_one")
       player_choice = get_player_choice
 
       if VALID_CHOICES.keys.include?(player_choice)
         player_choice = VALID_CHOICES[player_choice]
         break
       elsif player_choice == 's'
-        prompt "Please enter either 'sc' for scissors or 'sp' for spock."
+        prompt_text("sc_or_sp")
       else
-        prompt "That's not a valid choice. Please try again."
+        prompt_text("not_valid")
       end
     end
 
     computer_choice = VALID_CHOICES.values.sample
 
-    result_message = "You chose: #{player_choice}; the Computer chose: #{computer_choice}"
+    result_message = MESSAGES['choice'] % { player_choice: player_choice, computer_choice: computer_choice }
     result = display_results(player_choice, computer_choice)
     prompt result_message
     prompt result
@@ -93,18 +90,17 @@ loop do # main loop
     end
 
     system('clear')
-    prompt "The current score is Player: #{player_score} and Computer: #{computer_score}"
+    prompt MESSAGES["score"] % { player_score: player_score, computer_score: computer_score }
     prompt result_message
     prompt result
 
     break if grand_master?(player_score, computer_score)
   end
 
-  master_announcement(player_score, computer_score)
-  prompt "Would you like to play again? (y/n)"
+  master_announcement(player_score)
+  prompt_text("again")
   answer = get_player_choice
   break unless answer.downcase.start_with?('y')
-
 end
 
-prompt "Thank you for playing. Goodbye!"
+prompt_text("goodbye")
