ba69d039e493c1035ac3a6c750494e23ab561273
Blake Cutherell <bcutherell@gmail.com>
2024-10-15 20:51:31 -0500

    second pass through problems

---
+++ RB110/101-119_small_problems/easy_6/delete2.rb
@@ -0,0 +1,18 @@
+VOWELS = 'aeiouAEIOU'
+
+def remove_vowels(array)
+  # array.map do |word|
+  #   word.chars.select { |letter| !VOWELS.include?(letter)}.join
+  # end
+  array.map do |word|
+    word.delete(VOWELS)
+  end
+end
+
+p remove_vowels(%w(abcdefghijklmnopqrstuvwxyz)) #== %w(bcdfghjklmnpqrstvwxyz)
+p remove_vowels(%w(green YELLOW black white)) #== %w(grn YLLW blck wht)
+p remove_vowels(%w(ABC AEIOU XYZ)) #== ['BC', '', 'XYZ']
+
+p remove_vowels(%w(abcdefghijklmnopqrstuvwxyz)) == %w(bcdfghjklmnpqrstvwxyz)
+p remove_vowels(%w(green YELLOW black white)) == %w(grn YLLW blck wht)
+p remove_vowels(%w(ABC AEIOU XYZ)) == ['BC', '', 'XYZ']
