9927fbdbedb95dc32420bcd36397dc5057496790
Blake Cutherell <bcutherell@gmail.com>
2024-11-04 17:23:07 -0600

    second pass

---
+++ RB110/101-119_small_problems/easy_7/how_many2.rb
@@ -0,0 +1,11 @@
+def count_occurrences(array)
+  totals = array.tally
+  totals.each { |key, value| puts "#{key} => #{value}" }
+end
+
+vehicles = [
+  'car', 'car', 'truck', 'car', 'SUV', 'truck',
+  'motorcycle', 'motorcycle', 'car', 'truck', 'suv'
+]
+
+count_occurrences(vehicles)
