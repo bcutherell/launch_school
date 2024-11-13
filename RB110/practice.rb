4a17173acb5210e94fe72a213a18bfc8135acfe8
Blake Cutherell <bcutherell@gmail.com>
2024-11-13 16:38:50 -0600

    RB110 practice session

--- RB110/practice.rb
+++ RB110/practice.rb
@@ -1,135 +1,158 @@
-require 'yaml'
-MESSAGES = YAML.load_file('calculator_messages.yml') # Extracting Messages to a yaml.
+ALPHABET = 'abcdefghijklmnopqrstuvwxyz'
 
-def prompt(message)
-  puts ">> #{message}"
-end
-
-def valid_integer?(num)
-  num.to_i.to_s == num  # Already had done this when refactoring.
-end
-
-def valid_float?(num)
-  num.to_f.to_s == num  # Already had done this when refactoring.
-end
-
-def float?(num)
-  num.include?('.')
-end
-
-def operation_to_message(operator)
-  case operator
-  when '1'
-    DISPLAY['adding']
-  when '2'
-    DISPLAY['subtracting']
-  when '3'
-    DISPLAY['multiplying']
-  when '4'
-    DISPLAY['dividing']
-  end
-end
-
-loop do
-  valid_languages = %w[en es de]
-  prompt(MESSAGES['beginning']['select_language'])
-  language = gets.chomp.downcase
-
-  if valid_languages.include?(language) # Is it OK to set a CONSTANT with input? I don't know much about constants yet
-    LANGUAGE = language
-    DISPLAY = MESSAGES[LANGUAGE]
-    break
-  else
-    prompt(MESSAGES['beginning']['try_again'])
-  end
-end
-
-prompt(DISPLAY['welcome'])
-name = ''
-
-loop do
-  name = gets.chomp
-  break unless name.empty?
-
-  prompt(DISPLAY['valid_name'])
-end
-
-prompt("#{DISPLAY['hi']}, #{name}!")
-loop do # main loop
-  number1 = nil
-  number2 = nil
-
-  loop do
-    prompt(DISPLAY['enter_number'])
-    number1 = gets.chomp
-
-    if float?(number1)
-      valid_float?(number1)
-      number1 = number1.to_f
-      break
-    elsif valid_integer?(number1)
-      number1 = number1.to_i
-      break
-    else
-      prompt(DISPLAY['invalid_number'])
+def match_count(word)
+  counter = 0
+    word.chars.each_with_index do |letter, index|
+      if letter.downcase == ALPHABET[index]
+        counter += 1 
+      end
     end
-  end
-
-  loop do
-    prompt(DISPLAY['enter_number'])
-    number2 = gets.chomp
-
-    if float?(number2)
-      valid_float?(number2)
-      number2 = number2.to_f
-      break
-    elsif valid_integer?(number2)
-      number2 = number2.to_i
-      break
-    else
-      prompt(DISPLAY['invalid_number'])
-    end
-  end
-
-  prompt(DISPLAY['operator_prompt'])
-  operator = ''
-
-  loop do
-    operator = gets.chomp
-    break if %w[1 2 3 4].include?(operator)
-
-    prompt(DISPLAY['valid_operation'])
-  end
-
-  prompt("#{operation_to_message(operator)} #{DISPLAY['two_numbers']}")
-  sleep(0.5)
-
-  result = case operator
-           when '1'
-             number1 + number2
-           when '2'
-             number1 - number2
-           when '3'
-             number1 * number2
-           when '4'
-             if number2.zero?
-               prompt(DISPLAY['divide_by_zero_error'])
-
-               prompt(DISPLAY['again_question'])
-               answer = gets.chomp
-               break unless answer.downcase.start_with?('y')
-             else
-               number1 / number2.to_f
-             end
-           end
-
-  prompt("#{DISPLAY['the_result']} #{result}")
-  prompt(DISPLAY['again_question'])
-  answer = gets.chomp
-  break unless answer.downcase.start_with?('y')
+  counter
 end
 
-prompt(DISPLAY['thank_you'])
+def solve(array)
+  array.map { |word| match_count(word) }
+end
+
+p solve(["abode","ABc","xyzD"]) == [4,3,1] # true
+p solve(["abide","ABc","xyz"]) == [4,3,0] # true
+p solve(["IAMDEFANDJKL","thedefgh","xyzDEFghijabc"]) == [6,5,7] # true
+p solve(["encode","abc","xyzD","ABmD"]) == [1, 3, 1, 3] # true
+
+
+
+# require 'yaml'
+# MESSAGES = YAML.load_file('calculator_messages.yml') # Extracting Messages to a yaml.
+
+# def prompt(message)
+#   puts ">> #{message}"
+# end
+
+# def valid_integer?(num)
+#   num.to_i.to_s == num  # Already had done this when refactoring.
+# end
+
+# def valid_float?(num)
+#   num.to_f.to_s == num  # Already had done this when refactoring.
+# end
+
+# def float?(num)
+#   num.include?('.')
+# end
+
+# def operation_to_message(operator)
+#   case operator
+#   when '1'
+#     DISPLAY['adding']
+#   when '2'
+#     DISPLAY['subtracting']
+#   when '3'
+#     DISPLAY['multiplying']
+#   when '4'
+#     DISPLAY['dividing']
+#   end
+# end
+
+# loop do
+#   valid_languages = %w[en es de]
+#   prompt(MESSAGES['beginning']['select_language'])
+#   language = gets.chomp.downcase
+
+#   if valid_languages.include?(language) # Is it OK to set a CONSTANT with input? I don't know much about constants yet
+#     LANGUAGE = language
+#     DISPLAY = MESSAGES[LANGUAGE]
+#     break
+#   else
+#     prompt(MESSAGES['beginning']['try_again'])
+#   end
+# end
+
+# prompt(DISPLAY['welcome'])
+# name = ''
+
+# loop do
+#   name = gets.chomp
+#   break unless name.empty?
+
+#   prompt(DISPLAY['valid_name'])
+# end
+
+# prompt("#{DISPLAY['hi']}, #{name}!")
+# loop do # main loop
+#   number1 = nil
+#   number2 = nil
+
+#   loop do
+#     prompt(DISPLAY['enter_number'])
+#     number1 = gets.chomp
+
+#     if float?(number1)
+#       valid_float?(number1)
+#       number1 = number1.to_f
+#       break
+#     elsif valid_integer?(number1)
+#       number1 = number1.to_i
+#       break
+#     else
+#       prompt(DISPLAY['invalid_number'])
+#     end
+#   end
+
+#   loop do
+#     prompt(DISPLAY['enter_number'])
+#     number2 = gets.chomp
+
+#     if float?(number2)
+#       valid_float?(number2)
+#       number2 = number2.to_f
+#       break
+#     elsif valid_integer?(number2)
+#       number2 = number2.to_i
+#       break
+#     else
+#       prompt(DISPLAY['invalid_number'])
+#     end
+#   end
+
+#   prompt(DISPLAY['operator_prompt'])
+#   operator = ''
+
+#   loop do
+#     operator = gets.chomp
+#     break if %w[1 2 3 4].include?(operator)
+
+#     prompt(DISPLAY['valid_operation'])
+#   end
+
+#   prompt("#{operation_to_message(operator)} #{DISPLAY['two_numbers']}")
+#   sleep(0.5)
+
+#   result = case operator
+#            when '1'
+#              number1 + number2
+#            when '2'
+#              number1 - number2
+#            when '3'
+#              number1 * number2
+#            when '4'
+#              if number2.zero?
+#                prompt(DISPLAY['divide_by_zero_error'])
+
+#                prompt(DISPLAY['again_question'])
+#                answer = gets.chomp
+#                break unless answer.downcase.start_with?('y')
+#              else
+#                number1 / number2.to_f
+#              end
+#            end
+
+#   prompt("#{DISPLAY['the_result']} #{result}")
+#   prompt(DISPLAY['again_question'])
+#   answer = gets.chomp
+#   break unless answer.downcase.start_with?('y')
+# end
+
+# prompt(DISPLAY['thank_you'])
 
 
 
@@ -166,7 +189,7 @@ prompt(DISPLAY['thank_you'])
 # end
 
 # p flintstones_hash
-#-----------------------------
+# -----------------------------
 # ages = { "Herman" => 32, "Lily" => 30, "Grandpa" => 5843, "Eddie" => 10, "Marilyn" => 22, "Spot" => 237 }
 # ages_total = 0
 
@@ -179,13 +202,13 @@ prompt(DISPLAY['thank_you'])
 # end
 
 # p ages_total
-#-----------------------------
+# -----------------------------
 # ages = { "Herman" => 32, "Lily" => 30, "Grandpa" => 402, "Eddie" => 10 }
 
 # p ages.reject { |key, value| value >= 100 }
 # p ages.select { |key, value| value < 100}
 # p ages.delete_if { |key, value| value >= 100 }
-#------------------------------
+# ------------------------------
 
 # ages = { "Herman" => 32, "Lily" => 30, "Grandpa" => 5843, "Eddie" => 10, "Marilyn" => 22, "Spot" => 237 }
 # current_age = 0
@@ -196,7 +219,7 @@ prompt(DISPLAY['thank_you'])
 
 
 
-#-------------------------------
+# -------------------------------
 
 # munsters = {
 #   "Herman" => { "age" => 32, "gender" => "male" },
@@ -220,7 +243,7 @@ prompt(DISPLAY['thank_you'])
 
 # ---------------------------------
 
-#wiki problem about returning the number of pairs
+# wiki problem about returning the number of pairs
 
 # def pairs(arr)
 #   arr_uniq = arr.uniq
