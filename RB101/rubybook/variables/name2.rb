0c65e5c6afc18948942958bec883593cd888fc46
Blake Cutherell <bcutherell@gmail.com>
2024-11-14 17:06:36 -0600

    practice problems and moving files around

---
+++ RB101/rubybook/variables/name2.rb
@@ -0,0 +1,6 @@
+puts "==> Hello, please give me your first name."
+first_name = gets.chomp
+puts "==> Now your last name, please."
+last_name = gets.chomp
+
+puts "Hello #{first_name} #{last_name}, good to meet you!"
---
+++ RB101/rubybook/variables/three.rb
@@ -0,0 +1,9 @@
+puts "Tell me your name!"
+
+name = gets.chomp
+
+puts "Hello #{name}!"
+
+10.times do 
+	puts name
+end
