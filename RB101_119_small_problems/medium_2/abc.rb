7fafce529003d8153e4615114e4535383f7bdc3c
Blake Cutherell <bcutherell@gmail.com>
2024-12-05 08:33:39 -0600

    practice

---
+++ RB101_119_small_problems/medium_2/abc.rb
@@ -0,0 +1,28 @@
+BLOCKS = %w(BO XK DQ CP NA GT RE FS JW HU VI LY ZM)
+
+def block_word?(string)
+  array = string.upcase.chars
+  # BLOCKS.each do |block|
+  #   if array.include?(block[0]) && array.include?(block[1])
+  #     return false
+  #   elsif
+  #     array.include?(block[0]) || array.include?(block[1])
+  #     return true
+  #   end
+  # end
+  BLOCKS.each do |block|
+    p block
+  end
+end
+
+p block_word?('BATCH') #== true
+p block_word?('BUTCH') #== false
+p block_word?('jest') #== true
+p block_word?('apples') #== false
+p block_word?('Baby') #== false
+
+p block_word?('BATCH') == true
+p block_word?('BUTCH') == false
+p block_word?('jest') == true
+p block_word?('apples') == false
+p block_word?('Baby') == false
